fe9210c3f4e7ef98d5d631ea210535f3c613da1d
Stephane P. Pericat <contact@pericat.net>
2022-02-12 13:17:43 -0600

    feat: getFile

--- pages/index.vue
+++ pages/index.vue
@@ -16,12 +16,16 @@
 
 <script>
 import consola from 'consola'
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, onMounted } from '@nuxtjs/composition-api'
+
+import usePdfData from '~/composables/usePdfData'
 
 export default defineComponent({
   name: 'IndexPage',
 
-  setup() {
+  setup(_, { root }) {
+    const { $pdf } = root
+
     const onDownload = () => {
       consola.info('download...')
     }
@@ -29,6 +33,13 @@ export default defineComponent({
     const onPrint = () => {
       consola.info('print...')
     }
+
+    const { getFile } = usePdfData($pdf)
+
+    onMounted(async () => {
+      const doc = await getFile('/compressed.tracemonkey-pldi-09.pdf')
+      consola.info('mounted', doc)
+    })
 
     return {
       onDownload,
